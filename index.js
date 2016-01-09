68aea65a50c36fc0773ce399264a797679f94dd0
Asad Hasan <asad@dadesigners.com>
2016-01-09 14:56:50 -0600

    Go over all pages.

--- index.js
+++ index.js
@@ -22,15 +22,22 @@ function *run() {
   .wait();
 
   while(i <= perpage) {
+    if(i == 1) {
+      //Reset number of items per page.
+      perpage = yield nightmare.wait('#post_list').evaluate(function() {
+          return $('#post_list .post').length;
+      });
+    }
+
     item = yield nightmare
-    //.wait(500).screenshot('scr2.png')
+    //Wait for index page to load.
     .wait('.pagination')
-    //.wait(500).screenshot('scr3.png')
+    //Click read more button of current post.
     .click('#post_list .post:nth-child('+i*6+') .meta.last-child a')/*.wait('h1.ng-binding')*/
-    //.wait(500).screenshot('scr4.png')
     .wait(1000)
+    //Wait for third breadcrumb to be available, suggesting inside page has loaded.
     .wait('#breadcrumb li:nth-child(3)')
-    //.wait(500).screenshot('scr5.png')
+    //Extract Fields.
     .evaluate(function() {
       var item = {};
       item['body'] = $('.body').html();
@@ -39,12 +46,23 @@ function *run() {
     yield nightmare.back().wait('.pagination');
     items.push(item);
     console.log(item);
+    //On last item.
     if(i == perpage) {
+      //Figure out if we are on the last page.
       lastPage = yield nightmare.evaluate(function() {
         return !Boolean($('.pagination .next a').length);
       });
+
+      if(lastPage) {
+        console.log("Total Posts: "+j);
+      } else {
+        //Reset for next page and go.
+        i = 0;
+        yield nightmare.click('.pagination .next a');
+      }
     }
     i++;
+    j++;
   }
 
   yield nightmare.end();
