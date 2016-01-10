67406a9cde10d5b33d33343b5965196c3eeac317
Asad Hasan <asad@dadesigners.com>
2016-01-09 20:02:12 -0600

    Reset to begingin.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function *run() {
   var items = [];
   var lastPage = false;
   var firstPage = true;
-  var indexPage = "http://www.larrylipshultz.com/blog?page=7";
+  var indexPage = "http://www.larrylipshultz.com/blog";
   yield nightmare
   .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
   .goto(indexPage)
